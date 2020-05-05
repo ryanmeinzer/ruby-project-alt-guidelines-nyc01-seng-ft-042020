04aa21b230dce6cf24f8f4cbd68c4a96c99cd638
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2020-05-05 11:32:05 -0400

    adding some CLI greetings to start

---
+++ lib/app / models/greeting.rb
@@ -0,0 +1,13 @@
+def greeting(name)
+    puts "What up, #{name.chomp.strip}. Let's hit Trail 1 together. Saddle up and follow me. Remember to hit that stopwatch to track your time on this run."
+    puts "|          |"
+    puts " |        |"
+    puts "  |      |"
+    puts "   |    |"
+    puts "    |  |"
+    puts "Siiiick run. How fast did you do that run in? Just enter the amount of seconds."
+end
+
+def second_message(time)
+    puts "Whoa, you shred that trail in #{time.chomp.strip} seconds?! Sick bra. Let's hit another run, this time - trail 4."
+end
