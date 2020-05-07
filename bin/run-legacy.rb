6ad32b8dd910f33c808830f918033b1e22e91a81
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2020-05-06 23:35:51 -0400

    refactored all to meet MVP with CRUD, OO, Join Table and multifunctional user-driven gameplay

---
+++ bin/run-legacy.rb
@@ -0,0 +1,17 @@
+# require_relative '../config/environment'
+
+# puts "Yo, what's your name? Let's shred."
+# puts ""
+# puts ""
+
+# greeting
+
+# starter
+
+# play
+
+# puts ""
+# puts ""
+# puts "Peace!"
+# puts ""
+# puts ""
